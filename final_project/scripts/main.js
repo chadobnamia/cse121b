c686381cb3b5a2d3b35b53afaacba1dd8a9ae888
ChadDev2022 <richardobnamia@gmail.com>
2024-02-19 20:18:47 +0800

    fix json

--- final_project/scripts/main.js
+++ final_project/scripts/main.js
@@ -1,45 +1,18 @@
 const areaGeometry = document.querySelector('#geometry');
 let geometryDeets = [];
 
-updateParam = () =>{
-    const shapes = document.getElementById('shapes').value;
-    const paramDivs = document.getElementById('params');
-    paramDivs.innerHTML = shapeParams[shape].join("<br>");
+displayContent = (content) =>{
+    const jsonContent = document.getElementById("parameters");
+    jsonContent.innerHTML = content.input[0];
 }
 
-calculateArea = () =>{
-    const shapes = document.getElementById('shapes').value;
-    let area;
-    switch(shapes){
-        case "circle":
-            const radius = parseFloat(document.getElementById("radius").value);
-            area = Math.PI * radius * radius;
-            break;
-        case "rectangle":
-            const length = parseFloat(document.getElementById('length').value);
-            const width = parseFloat(document.getElementById('width').value);
-            area = length * width;
-            break;
-        case "triangle":
-            const base = parseFloat(document.getElementById("base").value);
-            const height = parseFloat(document.getElementById("height").value);
-            area = 0.5 * base * height;
-            break; 
-        case "square":
-            const side = parseFloat(document.getElementById('side').value);
-            area = side * side;
-            break;
-        default:
-            area="Invalid shape";
-    }
+const getData = async () =>{
+    const response = await fetch ('https://chadobnamia.github.io/cse121b/final_project/geometry.json');
+
+    geometryDeets = await response.json();
 }
-const getData = async () => {
-    // Fetch temple data
-    const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+getData();
+document.querySelector('#shapes').addEventListener('change', () => {
+    // Call the filterTemples function and pass an arrow function result with the templeList as the argument
+    displayContent(geometryDeets)});
 
-    // Convert response to JavaScript object
-    templeList = await response.json();
-
-    // Call displayTemples function and pass the templeList
-    displayTemples(templeList);
-};
